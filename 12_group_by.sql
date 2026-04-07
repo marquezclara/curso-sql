0d3334387dee3ab21556e7ae25b4196f47af9f7e
Maria Clara Marques <marquezmclara@gmailcom>
2026-04-07 12:24:43 -0300

    aula 18 - sql

--- 12_group_by.sql
+++ 12_group_by.sql
@@ -1,5 +1,25 @@
 --count de produtos agrupados por IdProduto
-SELECT IdProduto,
-        count(*)
-FROM transacao_produto
-GROUP BY IdProduto
+--group by é usado para agrupar linhas que têm os mesmos valores em colunas especificadas em linhas de resumo
+
+-- SELECT IdProduto,
+--         count(*)
+-- FROM transacao_produto
+-- GROUP BY IdProduto
+
+--cliente que mais juntou pontos no mês de julho
+SELECT idCliente,
+        sum(QtdePontos),
+        count(IdTransacao)
+
+FROM transacoes
+
+WHERE DtCriacao >= '2024-07-01' 
+AND DtCriacao < '2024-08-01'
+
+GROUP BY idCliente
+--having é usado para filtrar os resultados de um group by - where do group by - filtro da agregação
+HAVING sum(QtdePontos) > 2500
+--order by é sempre o último comando a ser executado, depois do group by e antes do limit (se houver)
+ORDER BY sum(QtdePontos) DESC
+--limit é o ultimo comando a ser executado, depois do order by
+LIMIT 10
